cb3e2b577032bc09efdcf2ac980b7052f1e1580a
anshmungelwar <anshmungelwar@gmail.com>
2026-03-21 22:57:36 +0530

    accessing required component

---
+++ script.js
@@ -0,0 +1,5 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let newBTN = document.querySelector(".newGame");
+let msg = document.querySelector(".msg");
+let msgContainer = document.querySelector(".msg-container");
