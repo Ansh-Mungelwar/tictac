c244ee65b904d8199dd949f8f61fea5e2daadf11
anshmungelwar <anshmungelwar@gmail.com>
2026-03-22 13:53:30 +0530

    add winning pattern

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let resetBtn = document.querySelector(".reset");
 let newBTN = document.querySelector(".newGame");
 let msg = document.querySelector(".msg");
 let msgContainer = document.querySelector(".msg-container");
+
+let turnO = true;
+
+const winPatttern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
